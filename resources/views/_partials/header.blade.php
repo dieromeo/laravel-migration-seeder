b2c2256ce2d17fffc86a1151f4ea14c1d7dcc614
dieromeo <diegoromeo99@icloud.com>
2024-01-30 16:17:38 +0100

    feat: train list

---
+++ resources/views/_partials/header.blade.php
@@ -0,0 +1,7 @@
+<header class="py-4">
+    <div class="container d-flex justify-content-center">
+        <div class="bg-dark-subtle p-4 rounded-5">
+            <h1 class="text-center text-uppercase fs-2">Treni in partenza oggi</h1>
+        </div>
+    </div>
+</header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-    <title>Template title</title>
-</head>
+@extends('_partials.head')
+@section('titolo_pagina')
+    Treni in partenza
+@endsection
 
 <body>
-    <h1>title</h1>
-    <!-- <img src="{{ Vite::asset('resources/img/laravel-logo.webp') }}" alt=""> -->
+    @include('_partials.header')
+    <main class="p-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                @foreach ($trains as $train)
+                    @if ($train['departure_date'] === date('Y-m-d'))
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <div class="card pt-3" style="width: 18rem;">
+                                <img src="{{ Vite::asset('resources/img/train.svg') }}" alt="train" id="train">
+                                <div class="card-body">
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">Parte da: {{ $train['departure_station'] }}</li>
+                                        <li class="list-group-item">Alle ore: {{ $train['departure_time'] }}</li>
+                                        <li class="list-group-item">Destinazione: {{ $train['arrival_station'] }}</li>
+                                        <li class="list-group-item">Alle ore: {{ $train['arrival_time'] }}</li>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
